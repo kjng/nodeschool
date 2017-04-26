1b2a98a85f8b7bdcf728e9a6531f6287571bb8a0
Kevin Jang <pianistkevinjang@gmail.com>
2017-04-26 08:26:40 -0700

    Complete http-client

---
+++ learnyounode/http-client.js
@@ -0,0 +1,9 @@
+var http = require('http');
+var url = process.argv[2];
+
+http.get(url, function(res) {
+  res.setEncoding('utf8');
+
+  res.on('error', console.error);
+  res.on('data', console.log);
+});
