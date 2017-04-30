e6064e7300d20c820ec6544fe4ce74ea0cc38df1
Kevin Jang <pianistkevinjang@gmail.com>
2017-04-29 20:59:54 -0700

    Complete json-api-server

---
+++ learnyounode/json-api-server.js
@@ -0,0 +1,64 @@
+var http = require('http');
+var url = require('url');
+
+var server = http.createServer(function(req, res) {
+  var requestUrl = url.parse(req.url, true);
+  var userDate = new Date(requestUrl.query.iso);
+
+  res.writeHead(200, { 'Content-Type': 'application/json' });
+
+  if (requestUrl.pathname === '/api/parsetime') {
+    res.end(JSON.stringify({
+      hour: userDate.getHours(),
+      minute: userDate.getMinutes(),
+      second: userDate.getSeconds()
+    }));
+  }
+
+  if (requestUrl.pathname === '/api/unixtime') {
+    res.end(JSON.stringify({
+      unixtime: userDate.getTime()
+    }));
+  }
+
+});
+
+server.listen(process.argv[2]);
+
+// Official solution:
+
+// var http = require('http')
+// var url = require('url')
+
+// function parsetime (time) {
+//   return {
+//     hour: time.getHours(),
+//     minute: time.getMinutes(),
+//     second: time.getSeconds()
+//   }
+// }
+
+// function unixtime (time) {
+//   return { unixtime: time.getTime() }
+// }
+
+// var server = http.createServer(function (req, res) {
+//   var parsedUrl = url.parse(req.url, true)
+//   var time = new Date(parsedUrl.query.iso)
+//   var result
+
+//   if (/^\/api\/parsetime/.test(req.url)) {
+//     result = parsetime(time)
+//   } else if (/^\/api\/unixtime/.test(req.url)) {
+//     result = unixtime(time)
+//   }
+
+//   if (result) {
+//     res.writeHead(200, { 'Content-Type': 'application/json' })
+//     res.end(JSON.stringify(result))
+//   } else {
+//     res.writeHead(404)
+//     res.end()
+//   }
+// })
+// server.listen(Number(process.argv[2]))
